1a9cda34ec329b2613d924e7674b8e069ac7dde9
Thread-ad <agris123@gmail.com>
2025-01-02 08:35:53 +0000

    Finished initial code and working on testing code

--- scr/main.py
+++ scr/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from textnode import TextType, TextNode
+
+
+def main():
+
+    test_book = TextNode("The Wizard's Guide", TextType.NORMAL_TEXT, "https://bootdev.com")
+
+    print(test_book)
+
+if __name__ == "__main__":
+    main()
