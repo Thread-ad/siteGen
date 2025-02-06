a59cc86e6c455674ea478fa3bbcd81e84694b7bf
Thread-ad <agris123@gmail.com>
2025-02-06 10:42:36 +0000

    Made a script for file organization

--- scr/main.py
+++ scr/main.py
@@ -1,11 +1,37 @@
 from textnode import TextType, TextNode
+import os, shutil
 
 
 def main():
+        
+    def public_move(directory,destination):
+        if os.path.exists(destination):
+            shutil.rmtree(destination)
+        os.mkdir(destination)
 
-    test_book = TextNode("The Wizard's Guide", TextType.NORMAL_TEXT, "https://bootdev.com")
+        paths = os.listdir(directory)
+        if paths == "":
+            return
 
-    print(test_book)
+        for path in paths:
+            print(f"path is: {path}")
+            print(f"     path is file: {os.path.isfile(path)}")
+            if os.path.isfile(path) == True:
+                new_dest = os.path.join("public",path)
+                print(f"New destination is {new_dest}")
+                if os.path.exists(new_dest) != True:
+                    os.mkdir(new_dest)
+                return public_move(os.path.join(directory, path),new_dest)
+
+                
+            shutil.copy(os.path.join(directory,path),destination)
+
+
+        
+        return 
+
+    return public_move("static","public")
+
 
 if __name__ == "__main__":
     main()
